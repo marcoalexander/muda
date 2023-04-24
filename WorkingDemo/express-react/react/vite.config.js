049665ffcdfc29b886ca5ce83c9b4cd4b007c8cf
marcoalexander <63327508+marcoalexander@users.noreply.github.com>
2023-04-24 13:14:40 -0500

    dockerized working demo

--- WorkingDemo/express-react/react/vite.config.js
+++ WorkingDemo/express-react/react/vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 3000,
     proxy: {
       // string shorthand
-      '/api': 'http://localhost:8080/',
+      '/api': 'http://app-server:8080/',
     }
   }
 })
